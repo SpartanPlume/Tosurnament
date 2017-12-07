4efd59a1a88356dde46e2e9ca845d60a22ba5821
SpartanPlume <robin.charpentier@epitech.eu>
2017-12-07 21:14:02 +0900

    Add more commands and update register

--- modules/tosurnament.py
+++ modules/tosurnament.py
@@ -11,6 +11,7 @@ import helpers.crypt
 import helpers.load_json
 from databases.user import User
 from databases.tournament import Tournament
+from databases.players_spreadsheet import PlayersSpreadsheet
 
 EMBED_COLOUR = 3447003
 
@@ -26,7 +27,10 @@ class Module(modules.module.BaseModule):
             "link": self.link,
             "auth": self.auth,
             "create_tournament": self.create_tournament,
+            "set_staff_channel": self.set_staff_channel,
             "set_admin_role": self.set_admin_role,
+            "set_referee_role": self.set_referee_role,
+            "set_player_role": self.set_player_role,
             "register": self.register
         }
         self.help_messages = collections.OrderedDict([])
@@ -38,7 +42,6 @@ class Module(modules.module.BaseModule):
         """Sends a private message to the command runner to link his account"""
         discord_id = message.author.id
         user = self.client.session.query(User).filter(User.discord_id == helpers.crypt.hash_str(discord_id)).first()
-        user = helpers.crypt.decrypt_obj(user)
         if user:
             if user.verified:
                 return (message.channel, self.get_string("link", "already_verified", self.client.prefix, self.prefix), None)
@@ -52,7 +55,7 @@ class Module(modules.module.BaseModule):
         code = secrets.token_urlsafe(16)
         if not user:
             user = User(discord_id=discord_id, osu_id=osu_id, verified=False, code=code)
-            self.client.session.add(helpers.crypt.encrypt_obj(user))
+            self.client.session.add(user)
         else:
             user.osu_id = osu_id
             user.code = code
@@ -63,7 +66,6 @@ class Module(modules.module.BaseModule):
         """Sends a private message to the command runner to auth his account"""
         discord_id = message.author.id
         user = self.client.session.query(User).filter(User.discord_id == helpers.crypt.hash_str(discord_id)).first()
-        user = helpers.crypt.decrypt_obj(user)
         if not user:
             return (message.channel, self.get_string("auth", "not_linked", self.client.prefix, self.prefix), None)
         if user.verified:
@@ -85,7 +87,6 @@ class Module(modules.module.BaseModule):
             return (message.author, self.get_string("auth", "wrong_code", self.client.prefix, self.prefix), None)
         else:
             user.verified = True
-            user = helpers.crypt.encrypt_obj(user)
             self.client.session.commit()
         return (message.author, self.get_string("auth", "success"), None)
 
@@ -96,29 +97,85 @@ class Module(modules.module.BaseModule):
             return (message.channel, self.get_string("create_tournament", "usage", self.client.prefix, self.prefix), None)
         if not message.server:
             return (message.channel, self.get_string("create_tournament", "not_on_a_server"), None)
+        if message.server.owner != message.author:
+            return (message.channel, self.get_string("create_tournament", "not_owner"), None)
         tournament = self.client.session.query(Tournament).filter(Tournament.server_id == helpers.crypt.hash_str(message.server.id)).filter(Tournament.acronym == parameters[0]).first()
         if tournament:
             return (message.channel, self.get_string("create_tournament", "acronym_used"), None)
         tournament = Tournament(server_id=message.server.id, acronym=parameters[0], name=parameters[1])
-        self.client.session.add(helpers.crypt.encrypt_obj(tournament))
+        self.client.session.add(tournament)
         self.client.session.commit()
         return (message.channel, self.get_string("create_tournament", "success"), None)
+
+    async def set_staff_channel(self, message, parameter):
+        """Set the staff channel"""
+        if not message.server:
+            return (message.channel, self.get_string("set_staff_channel", "not_on_a_server"), None)
+        if len(message.channel_mentions) == 1:
+            tournament = self.client.session.query(Tournament).filter(Tournament.server_id == helpers.crypt.hash_str(message.server.id)).first()
+            if not tournament:
+                return (message.channel, self.get_string("set_staff_channel", "no_tournament", self.client.prefix, self.prefix), None)
+            if not tournament.admin_role_id and message.server.owner != message.author:
+                return (message.channel, self.get_string("set_staff_channel", "no_rights", self.client.prefix, self.prefix), None)
+            if message.server.owner != message.author and not any(role.id == tournament.admin_role_id for role in message.author.roles):
+                return (message.channel, self.get_string("set_staff_channel", "no_rights", self.client.prefix, self.prefix), None)
+            tournament.staff_channel_id = message.channel_mentions[0].id
+            self.client.session.commit()
+        else:
+            return (message.channel, self.get_string("set_staff_channel", "usage", self.client.prefix, self.prefix), None)
+        return (message.channel, self.get_string("set_staff_channel", "success"), None)
 
     async def set_admin_role(self, message, parameter):
         """Set the admin role"""
         if not message.server:
             return (message.channel, self.get_string("set_admin_role", "not_on_a_server"), None)
+        if message.server.owner != message.author:
+            return (message.channel, self.get_string("set_admin_role", "not_owner"), None)
         if len(message.role_mentions) == 1:
             tournament = self.client.session.query(Tournament).filter(Tournament.server_id == helpers.crypt.hash_str(message.server.id)).first()
             if not tournament:
                 return (message.channel, self.get_string("set_admin_role", "no_tournament", self.client.prefix, self.prefix), None)
-            tournament = helpers.crypt.decrypt_obj(tournament)
             tournament.admin_role_id = message.role_mentions[0].id
-            tournament = helpers.crypt.encrypt_obj(tournament)
             self.client.session.commit()
         else:
             return (message.channel, self.get_string("set_admin_role", "usage", self.client.prefix, self.prefix), None)
         return (message.channel, self.get_string("set_admin_role", "success"), None)
+
+    async def set_referee_role(self, message, parameter):
+        """Set the referee role"""
+        if not message.server:
+            return (message.channel, self.get_string("set_referee_role", "not_on_a_server"), None)
+        if len(message.role_mentions) == 1:
+            tournament = self.client.session.query(Tournament).filter(Tournament.server_id == helpers.crypt.hash_str(message.server.id)).first()
+            if not tournament:
+                return (message.channel, self.get_string("set_referee_role", "no_tournament", self.client.prefix, self.prefix), None)
+            if not tournament.admin_role_id and message.server.owner != message.author:
+                return (message.channel, self.get_string("set_referee_role", "no_rights", self.client.prefix, self.prefix), None)
+            if message.server.owner != message.author and not any(role.id == tournament.admin_role_id for role in message.author.roles):
+                return (message.channel, self.get_string("set_referee_role", "no_rights", self.client.prefix, self.prefix), None)
+            tournament.player_referee_id = message.role_mentions[0].id
+            self.client.session.commit()
+        else:
+            return (message.channel, self.get_string("set_referee_role", "usage", self.client.prefix, self.prefix), None)
+        return (message.channel, self.get_string("set_referee_role", "success"), None)
+
+    async def set_player_role(self, message, parameter):
+        """Set the player role"""
+        if not message.server:
+            return (message.channel, self.get_string("set_player_role", "not_on_a_server"), None)
+        if len(message.role_mentions) == 1:
+            tournament = self.client.session.query(Tournament).filter(Tournament.server_id == helpers.crypt.hash_str(message.server.id)).first()
+            if not tournament:
+                return (message.channel, self.get_string("set_player_role", "no_tournament", self.client.prefix, self.prefix), None)
+            if not tournament.admin_role_id and message.server.owner != message.author:
+                return (message.channel, self.get_string("set_player_role", "no_rights", self.client.prefix, self.prefix), None)
+            if message.server.owner != message.author and not any(role.id == tournament.admin_role_id for role in message.author.roles):
+                return (message.channel, self.get_string("set_player_role", "no_rights", self.client.prefix, self.prefix), None)
+            tournament.player_role_id = message.role_mentions[0].id
+            self.client.session.commit()
+        else:
+            return (message.channel, self.get_string("set_player_role", "usage", self.client.prefix, self.prefix), None)
+        return (message.channel, self.get_string("set_player_role", "success"), None)    
 
     async def register(self, message, parameter):
         """Registers a player"""
@@ -126,7 +183,6 @@ class Module(modules.module.BaseModule):
             return (message.channel, self.get_string("register", "not_on_a_server"), None)
         discord_id = message.author.id
         user = self.client.session.query(User).filter(User.discord_id == helpers.crypt.hash_str(discord_id)).first()
-        user = helpers.crypt.decrypt_obj(user)
         if not user:
             return (message.channel, self.get_string("register", "not_linked", self.client.prefix, self.prefix), None)
         if not user.verified:
@@ -140,17 +196,34 @@ class Module(modules.module.BaseModule):
             await self.client.change_nickname(message.author, osu_name)
         except discord.Forbidden:
             return (message.channel, self.get_string("register", "change_nickname_forbidden"), None)
+        tournament = self.client.session.query(Tournament).filter(Tournament.server_id == helpers.crypt.hash_str(message.server.id)).first()
+        if not tournament:
+            return (message.channel, self.get_string("register", "no_tournament", self.client.prefix, self.prefix), None)
         players = api.spreadsheet.get_range("1xRkVAPECBrH_alxfuBAhjXVjIzO0bk6CWd7P8Ch4P5k", "A2:A")
         for player_row in players:
             for player in player_row:
                 if osu_name == player:
+                    player_role = tournament.player_role_id
                     roles = message.server.roles
-                    for role in roles:
-                        if role.name == "Player":
-                            try:
-                                self.client.add_roles(message.author, role)
-                            except discord.Forbidden:
-                                return (message.channel, self.get_string("register", "change_role_forbidden", "Player"), None)
-                            return (message.channel, self.get_string("register", "success"), None)
-                    return (message.channel, self.get_string("register", "no_player_role"), None)
+                    if not player_role:
+                        for role in roles:
+                            if role.name == "Player":
+                                player_role = role
+                                break
+                    else:
+                        role_id = player_role
+                        player_role = None
+                        print(role_id)
+                        for role in roles:
+                            print(role.id)
+                            if role.id == role_id:
+                                player_role = role
+                                break
+                    if not player_role:
+                        return (message.channel, self.get_string("register", "no_player_role"), None)
+                    try:
+                        await self.client.add_roles(message.author, player_role)
+                    except discord.Forbidden:
+                        return (message.channel, self.get_string("register", "change_role_forbidden", player_role.name), None)
+                    return (message.channel, self.get_string("register", "success"), None)
         return (message.channel, self.get_string("register", "not_a_player"), None)
