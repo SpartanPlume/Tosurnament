c9e95d69b07716dc6896d69f569ed349c360594e
SpartanPlume <spartan.plume@gmail.com>
2024-02-13 19:43:38 +0100

    Add server with health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use tosurnament::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind port");
+    run(listener)?.await
 }
