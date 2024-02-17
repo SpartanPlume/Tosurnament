0c3cf8aa3487ad7f087245bfca1cc032bb29e608
Spartan Plume <spartan.plume@gmail.com>
2024-02-17 15:09:04 +0100

    Re-add scripts folder

---
+++ scripts/init_db.sh
@@ -0,0 +1,45 @@
+set -x
+set -eo pipefail
+
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error: psql is not installed."
+    echo >&2 "Use: cargo install --version="~0.7" sqlx-cli --no-default-features --features rustls,postgres"
+    exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed."
+    exit 1
+fi
+
+DB_USER=${POSTGRES_USER:=postgres}
+DB_PASSWORD=${POSTGRES_PASSWORD:=password}
+DB_NAME=${POSTGRES_DB:=tosurnament}
+DB_PORT=${POSTGRES_PORT:=5432}
+DB_HOST=${POSTGRES_HOST:=localhost}
+
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+    docker run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -p "${DB_PORT}":5432 \
+        -d postgres \
+        postgres -N 1000
+fi
+
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d postgres -c '\q'; do
+    echo >&2 "Postgress is still unavailable - sleeping"
+    sleep 1
+done
+
+echo >&2 "Postgres is up and running on port ${DB_PORT}!"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
+sqlx migrate run
+
+echo >&2 "Postgres has been migrated, ready to go!"
